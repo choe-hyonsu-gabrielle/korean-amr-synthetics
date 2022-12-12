39a96d54bc6550753b4703d0c40dce608adc0216
choe-hyonsu-gabrielle <choehyonsu@gmail.com>
2022-12-13 00:18:04 +0900

    최소한의 파일 open 최적화 필요함. optimal scenario: 모든 파일이 단 1번만 열리도록 하는 것.

--- synthetics/indexer.py
+++ synthetics/indexer.py
@@ -1,19 +1,18 @@
 import glob
-import random
 from collections import defaultdict
 from tqdm import tqdm
 from synthetics.utils import load_json
 
 
 class AnnotationPivot:
-    def __init__(self, ref_id):
+    def __init__(self, ref_id: str):
         self.ref_id = ref_id
-        self.texts = defaultdict(str)
+        self.texts = set()
         self.layers = defaultdict(str)
         self.complete = None
 
     def __len__(self):
-        return len(self.layers)
+        return len(self.texts)
 
     def __repr__(self):
         return f'<{self.__class__.__name__} → text="{self.texts}" | layers={self.layers}>'
@@ -22,14 +21,15 @@ class AnnotationPivot:
         return sorted(list(self.layers))
 
     def append(self, layer: str, locator: str, text: str):
-        self.texts[layer] = text
         self.layers[layer] = locator
+        self.texts.add(text)
 
 
 class AnnotationPivotIndexer:
     def __init__(self, layer_dirs: dict[str, str], file_extension='json'):
         self.layer_dirs = layer_dirs
         self.layer_files = {k: glob.glob(v + f'\\*.{file_extension}') for k, v in layer_dirs.items()}
+        self.doc_index = defaultdict(list)
         self.annotations = dict()
 
     def __repr__(self):
@@ -37,18 +37,17 @@ class AnnotationPivotIndexer:
         return f'<{self.__class__.__name__} → {len(layers)} layers: {layers}>'
 
     @property
-    def complete_items(self):
-        return list({k: v for k, v in self.annotations.items() if v.complete}.items())
+    def complete_item_keys(self):
+        return list({k for k, v in self.annotations.items() if v.complete})
 
     def index(self):
         counts = defaultdict(int)
-        processed_layers = []
+        layers_set = set()
 
         for layer, files in self.layer_files.items():
-            processed_layers.append(layer)
+            layers_set.add(layer)
             for filename in tqdm(files, desc=f'- indexing {len(files)} files from `{layer}`({self.layer_dirs[layer]})'):
-                data = load_json(filename)
-                for document_instance in data['document']:
+                for document_instance in load_json(filename)['document']:
                     for sentence_instance in document_instance['sentence']:
                         ref_id = sentence_instance['id']
                         text = sentence_instance['form']
@@ -56,13 +55,13 @@ class AnnotationPivotIndexer:
                             pivot = self.annotations[ref_id]
                             pivot.append(layer=layer, locator=filename, text=text)
                         else:
-                            pivot = AnnotationPivot(ref_id)
+                            pivot = AnnotationPivot(ref_id=ref_id)
                             pivot.append(layer=layer, locator=filename, text=text)
                             self.annotations[ref_id] = pivot
 
         for key in self.annotations:
             layers = self.annotations[key].get_layers()
-            self.annotations[key].complete = True if set(layers) == set(processed_layers) else False
+            self.annotations[key].complete = True if set(layers) == layers_set else False
             counts['-'.join(layers)] += 1
 
         print(f'- {len(self.annotations)} annotations so far: {dict(counts)}')
@@ -73,11 +72,9 @@ if __name__ == '__main__':
     dirs = {layer.split('\\')[-1]: layer for layer in glob.glob(search_space)}
 
     indexer = AnnotationPivotIndexer(layer_dirs=dirs)
-    print(indexer)
 
     indexer.index()
 
-    for idx, anno in random.sample(indexer.complete_items, k=10):
-        print(anno)
+    completes = indexer.complete_item_keys
 
-
+    print(completes)
