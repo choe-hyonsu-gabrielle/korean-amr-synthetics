b3b6a0cf1b6eb612826ab32757cab83fbc96735d
choe-hyonsu-gabrielle <choehyonsu@gmail.com>
2023-01-17 00:23:36 +0900

    update_from_mwe() 성공!

--- synthetics/main.py
+++ synthetics/main.py
@@ -1,6 +1,7 @@
 import glob
 from typing import Union
 from os.path import exists
+from pprint import pprint
 from synthetics.primitives.corpus import Corpus
 from synthetics.primitives.amr import AbstractMeaningRepresentation
 
@@ -17,23 +18,35 @@ def load_corpus(data_files: Union[str, list[str]], reload: bool = False, pickle_
 
 if __name__ == '__main__':
     # macOS: '/Users/choe.hyonsu.gabrielle/modu-corenlp-essential/layers-complete/*/*.json'
-
     search_space = 'D:/Corpora & Language Resources/modu-corenlp/layers-complete/*/*.json'
     corpus = load_corpus(data_files=search_space)
 
     len_between = (20, 50)
     stopwords = '\"\',“”‘’…;[]()<>'
 
+    fp = open('outputs.txt', encoding='utf-8', mode='w')
+
     for i, snt in enumerate(corpus.filter_by(len_range=len_between, exclude=stopwords, random_state=803)):
         print('\n\n')
         print(snt.annotations.dep)
         print(snt.annotations.srl)
         print(snt.annotations.el)
-        print(snt.annotations.pos.tostring())
         amr = AbstractMeaningRepresentation(annotations=snt.annotations)
+        amr.metadata['pos'] = snt.annotations.pos.tostring()
+        pprint(amr.graph.instances)
+        pprint(amr.graph.relations)
         print(amr.encode())
         print()
-        print(amr.graph.instances)
-        print(amr.graph.relations)
-        if i > 30:
+
+        print(snt.annotations.dep, file=fp)
+        print(snt.annotations.srl, file=fp)
+        print(snt.annotations.el, file=fp)
+        print(snt.annotations.cr, file=fp)
+        print(snt.annotations.za, file=fp)
+        print(amr.encode(), file=fp)
+        print('\n\n', file=fp)
+
+        if i > 50:
             break
+
+    fp.close()
